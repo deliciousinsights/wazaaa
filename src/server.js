8387f766313cacc8e807814db5ce5a78e2e94440
Christophe Porteneuve <tdd@tddsworld.com>
2016-03-20 21:22:24 +0100

    Étape 2 : passage à Express et Jade

--- src/server.js
+++ src/server.js
@@ -1,10 +1,27 @@
 import chalk from 'chalk'
+import express from 'express'
 import { createServer } from 'http'
+import createLogger from 'morgan'
+import path from 'path'
 
-const server = createServer((req, res) => {
-  res.end('WAZAAA!')
-})
+const app = express()
+const server = createServer(app)
+const PORT = Number(process.env.PORT) || 3000
+const isDev = app.get('env') === 'development'
 
-server.listen(3000, () => {
-  console.log(chalk`{green ✔ Server listening on port} {cyan 3000}`)
+app.set('views', path.resolve(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use(createLogger(isDev ? 'dev' : 'combined'))
+
+app.locals.title = 'Wazaaa'
+
+if (isDev) {
+  app.locals.pretty = true
+}
+
+app.get('/', (req, res) => res.render('home'))
+
+server.listen(PORT, () => {
+  console.log(chalk`{green ✔ Server listening on port} {cyan ${PORT}}`)
 })
